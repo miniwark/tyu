b2c7d0e93f72815448a24b0b99ecdcbcea391bb1
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-10 20:04:23 +0200

    Initial commit

---
+++ disk.go
@@ -0,0 +1,11 @@
+package main
+
+// `disks` represents the physical disk usage statistics
+type disks struct {
+}
+
+// return disk usage statistics
+func getDisks() disks {
+	result := disks{}
+	return result
+}
