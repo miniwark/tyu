e19b94f5fc84b754b160117fe7532c440bd8c45a
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-11 12:56:51 +0200

    Initial commit

---
+++ utils.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+// convert a []int8 array to a string
+// inspired by perterGo: https://groups.google.com/forum/#!topic/golang-nuts/Jel8Bb-YwX8
+func int8ArrayToString(char []int8) string {
+
+	s := make([]string, len(char))
+
+	for i := range s {
+		s[1] = strconv.QuoteRune(rune(char[i])) // convert each int8 char to a rune
+	}
+	return strings.Join(s, "")
+}
