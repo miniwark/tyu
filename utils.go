38ce7893e8b074328a473cbc6e781ec2086973c1
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-12 13:15:22 +0200

    Fix and improve the convertion to a string

--- utils.go
+++ utils.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
+import "strings"
 
-// convert and concatenate a []int8 array to a string
+// Convert and concatenate a []int8 slice to a string
 // inspired by perterGo: https://groups.google.com/forum/#!topic/golang-nuts/Jel8Bb-YwX8
-func int8ArrayToString(char []int8) string {
-
+func int8SliceToString(char []int8) string {
 	s := make([]string, len(char))
-
 	for i := range s {
-		s[1] = strconv.QuoteRune(rune(char[i])) // convert each int8 char to a rune
+		s[i] = string(char[i])
 	}
 	return strings.Join(s, "")
 }
+
+// TODO maybe change this to convert exclusively [65]int8 array ?
