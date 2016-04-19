40930dded0cbdf71459d40dc3eb8ec03f82f5710
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-19 18:47:10 +0200

    Update netinfo stats

--- net.go
+++ net.go
@@ -6,22 +6,24 @@ import (
 	"github.com/shirou/gopsutil/net"
 )
 
-// `netinfo` represent the network trafic informations
-type netinfo struct {
+// Netinfo represent the network trafic informations
+type Netinfo struct {
 	up   string
 	down string
 }
 
 // Get informations about the net trafic
-func getNetinfo() netinfo {
+func getNetinfo() Netinfo {
 	ioconters, err := net.IOCounters(false)
 	if err != nil {
 		panic(err) //TODO do not panic but manage the error
 	}
 
-	ret := netinfo{
-		up:   strconv.FormatUint(ioconters[1].BytesSent, 10),
-		down: strconv.FormatUint(ioconters[1].BytesRecv, 10),
+	ret := Netinfo{
+		//up:   strconv.FormatUint(ioconters[0].BytesSent, 10),
+		up: strconv.FormatFloat(float64(ioconters[0].BytesSent)/(1024*1024), 'f', 2, 64),
+		//down: strconv.FormatUint(ioconters[0].BytesRecv, 10),
+		down: strconv.FormatFloat(float64(ioconters[0].BytesRecv)/(1024*1024), 'f', 2, 64),
 	}
 	return ret
 }
