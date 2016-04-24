2b562d06fb28d5aede05125e787344b6806fd0be
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-24 22:14:03 +0200

    Update testing

--- mem_test.go
+++ mem_test.go
@@ -1,20 +1,72 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 
+	"github.com/shirou/gopsutil/mem"
 	"github.com/stretchr/testify/assert"
 )
 
+// TestGetMeminfo test the returned fields values of `getMeminfo()`
 func TestGetMeminfo(t *testing.T) {
+	// setup the faking of memVirtualMemory() & memSwapMemory
+	oldMemVirtualMemory := memVirtualMemory
+	OldMemSwapMemory := memSwapMemory
+	memVirtualMemory = func() (*mem.VirtualMemoryStat, error) {
+		ret := &mem.VirtualMemoryStat{
+			Total:       uint64(1024 * 1024 * 1024), // KiB to GiB conversion is implicitly tested --> ((1024 * 1024 * 1024) / (1024 * 1024 * 1024)) = 1.00
+			Used:        uint64(1024 * 1024 * 1024),
+			UsedPercent: float64(100),
+		}
+		return ret, nil
+	}
+	memSwapMemory = func() (*mem.SwapMemoryStat, error) {
+		ret := &mem.SwapMemoryStat{
+			Total:       uint64(1024 * 1024 * 1024),
+			Used:        uint64(1024 * 1024 * 1024),
+			UsedPercent: float64(100),
+		}
+		return ret, nil
+	}
+
+	// test
 	expected := meminfo{
-		ramTotal:        string(123),
-		ramUsed:         string(123),
+		ramTotal:        strconv.FormatFloat(1.00, 'f', 2, 64),
+		ramUsed:         strconv.FormatFloat(1.00, 'f', 2, 64),
 		ramUsedPercent:  int(100),
-		swapTotal:       string(123),
-		swapUsed:        string(123),
+		swapTotal:       strconv.FormatFloat(1.00, 'f', 2, 64),
+		swapUsed:        strconv.FormatFloat(1.00, 'f', 2, 64),
 		swapUsedPercent: int(100),
 	}
 	actual := getMeminfo()
-	assert.IsType(t, expected, actual, "`getMeminfo()` should return a `meminfo` type")
+	assert.Equal(t, expected, actual, "`getMeminfo` should be equal to --> Netinfo{ramTotal:\"1.00\", ramUsed:\"100\", ramUsedPercent:100, swapTotal:\"1.00\", swapUsed:\"1.00\", swapUsedPercent:100}")
+
+	// teardown
+	memVirtualMemory = oldMemVirtualMemory
+	memSwapMemory = OldMemSwapMemory
 }
+
+// TestGetMeminfoType test if `getMeminfo()` return a `meminfo` type and if each fields
+// of `meminfo` have the correct types
+// Types conversions & regression testing
+func TestGetMeminfoType(t *testing.T) {
+	expected := meminfo{
+		ramTotal:        string(0), // the result values of the `meminfo` fields are not tested
+		ramUsed:         string(0),
+		ramUsedPercent:  int(0),
+		swapTotal:       string(0),
+		swapUsed:        string(0),
+		swapUsedPercent: int(0),
+	}
+	actual := getMeminfo()
+	assert.IsType(t, expected, actual, "`getMeminfo()` should return a `meminfo` type")
+	assert.IsType(t, expected.ramTotal, actual.ramTotal, "`getMeminfo()` should return a `ramTotal` field with a string type")
+	assert.IsType(t, expected.ramUsed, actual.ramUsed, "`getMeminfo()` should return a `ramUsed` field with a string type")
+	assert.IsType(t, expected.ramUsedPercent, actual.ramUsedPercent, "`getMeminfo()` should return a `ramUsedPercent` field with an int type")
+	assert.IsType(t, expected.swapTotal, actual.swapTotal, "`getMeminfo()` should return a `swapTotal` field with a string type")
+	assert.IsType(t, expected.swapUsed, actual.swapUsed, "`getMeminfo()` should return a `swapUsed` field with a string type")
+	assert.IsType(t, expected.swapUsedPercent, actual.swapUsedPercent, "`getMeminfo()` should return a `swapUsedPercent` field with an int type")
+}
+
+//TODO add tests for errors --> must return empty/zero values
--- net_test.go
+++ net_test.go
@@ -3,16 +3,49 @@ package main
 import (
 	"testing"
 
+	"github.com/shirou/gopsutil/net"
 	"github.com/stretchr/testify/assert"
 )
 
+// TestGetNetinfo test the returned fields values of `getNetinfo()`
 func TestGetNetinfo(t *testing.T) {
+	// setup the faking of net.IOCounters()
+	oldNetIocounters := netIocounters
+	netIocounters = func(pernic bool) ([]net.IOCountersStat, error) {
+		ret := []net.IOCountersStat{
+			{
+				BytesSent: uint64(1024), // Bytes to KiB conversion is implicitly tested --> (1024 / 1024) = 1
+				BytesRecv: uint64(1024),
+			},
+		}
+		return ret, nil
+	}
+
+	// test
 	expected := Netinfo{
-		up:   float64(123.123),
-		down: float64(123.123),
+		up:   float64(1),
+		down: float64(1),
 	}
 	actual := getNetinfo()
-	assert.IsType(t, expected, actual, "`getNetinfo()` should return a `Netinfo` type")
-	//assert.Equal(t, expected, actual, "`getNetinfo()` should be equal to --> Netinfo{up: 123.123, down: 123.123}")
-	//TODO add mock or monckey patching to test equality
+
+	assert.Equal(t, expected, actual, "`getNetinfo()` should be equal to --> Netinfo{up:1, down:1}")
+	// teardown
+	netIocounters = oldNetIocounters
 }
+
+// TestGetNetinfoType test if `getNetinfo()` return a `Netinfo` type and if each fields
+// of `Netinfo` have the correct types
+// Types conversions & regression testing
+func TestGetNetinfoType(t *testing.T) {
+	expected := Netinfo{
+		up:   float64(0), // the result values of the `Netinfo` fields are not tested
+		down: float64(0),
+	}
+	actual := getNetinfo()
+
+	assert.IsType(t, expected, actual, "`getNetinfo()` should return a `Netinfo` type")
+	assert.IsType(t, expected.up, actual.up, "`getNetinfo()` should return a `up` field with a float64 type")
+	assert.IsType(t, expected.down, actual.down, "`getNetinfo()` should return a `down` field with a float64 type")
+}
+
+//TODO add tests for errors --> must return empty/zero values
