902ac5c2645f4e760d2770bf5e8db1e1ba811eb8
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-25 23:00:57 +0200

    Update testing

--- host_test.go
+++ host_test.go
@@ -1,43 +1,60 @@
 package main
 
 import (
+	"syscall"
 	"testing"
 
+	"github.com/shirou/gopsutil/host"
 	"github.com/stretchr/testify/assert"
 )
 
 // TODO finish this test
 // TestGetMeminfo test the returned fields values of `getHostinfo()`
-// func TestGetHostinfo(t *testing.T) {
-// 	// setup the faking of `host.Info()`
-// 	oldHostInfo := hostInfo
-// 	hostInfo = func() (*host.InfoStat, error) {
-// 		ret := &host.InfoStat{
-// 			Hostname:        "hostname",
-// 			OS:              "Os",
-// 			Platform:        "Platform",
-// 			PlatformVersion: "10.00",
-// 		}
-// 		return ret, nil
-// 	}
-//
-// 	// test
-// 	expected := hostinfo{
-// 		hostname:        "hostname",
-// 		os:              "Os",
-// 		platform:        "Platform",
-// 		platformVersion: "10.00",
-// 		domainname:      "domainname",
-// 		osRelease:       "1.0.0",
-// 		arch:            "x86_64",
-// 	}
-// 	actual := getHostinfo()
-//
-// 	assert.Equal(t, expected, actual, "`getHostinfo()` should be equal to --> hostinfo{hostname:\"hostname\", domainname:\"domainname\", os:\"Os\", osRelease:\"1.0.0\", platform:\"Platform\", platformVersion:\"10.00\", arch:\"x86_64\"}")
-//
-// 	// teardown
-// 	hostInfo = oldHostInfo
-// }
+func TestGetHostinfo(t *testing.T) {
+	// setup the faking of `host.Info()`
+	oldHostInfo := hostInfo
+	hostInfo = func() (*host.InfoStat, error) {
+		ret := &host.InfoStat{
+			Hostname:        "abc",
+			OS:              "linux", // we need a `Linux` OS if we want to test datas retrivied by `getUname()`
+			Platform:        "abc",
+			PlatformVersion: "abc",
+		}
+		return ret, nil
+	}
+	oldgetUname := getUname
+	getUname = func() (syscall.Utsname, error) {
+		int8string := [65]int8{ // See the ASCII table: 97 = "a" ; 98 ="b" ; 99 = "c"
+			64, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+			64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+			64, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
+		} //TODO if possible avoid this complicate way to test because of [65]int8 requirement by syscall.Utsname
+		ret := syscall.Utsname{
+			Release:    int8string,
+			Machine:    int8string,
+			Domainname: int8string,
+		}
+		return ret, nil
+	}
+
+	// test
+	expected := hostinfo{
+		hostname:        "abc",
+		os:              "Linux",
+		platform:        "Abc",
+		platformVersion: "abc",
+		domainname:      "@0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ@abcdefghijklmnopqrstuvwxyz", //
+		osRelease:       "@0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ@abcdefghijklmnopqrstuvwxyz", //
+		arch:            "@0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ@abcdefghijklmnopqrstuvwxyz", //
+	}
+	actual := getHostinfo()
+
+	assert.Equal(t, expected, actual, "`getHostinfo()` should be equal to main.hostinfo{hostname:\"hostname\", domainname:\"domainname\", os:\"Os\", osRelease:\"1.0.0\", platform:\"Platform\", platformVersion:\"10.00\", arch:\"x86_64\"}")
+
+	// teardown
+	hostInfo = oldHostInfo
+	getUname = oldgetUname
+}
 
 // TestGetHostinfoType test if `getHostinfo()` return a `procinfo` type and if each fields have the correct types
 func TestGetHostinfoType(t *testing.T) {
