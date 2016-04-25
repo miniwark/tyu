5a3b253a67dc4531e373e2f22c4be583e665d833
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-25 17:15:15 +0200

    Update testing

--- bios_test.go
+++ bios_test.go
@@ -6,14 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetBIOSinfo(t *testing.T) {
+// TestGetBIOSinfoType test if `getBIOSinfo()` return a `biosinfo` type and if each fields have the correct types
+// Types regression testing
+func TestGetBIOSinfoType(t *testing.T) {
 	expected := biosinfo{
-		boardName:   "abc",
-		boardVendor: "abc",
-		biosVendor:  "abc",
-		biosVersion: "abc",
-		biosDate:    "abc",
+		boardName:   "", // the result values of the fields are not tested
+		boardVendor: "",
+		biosVendor:  "",
+		biosVersion: "",
+		biosDate:    "",
 	}
 	actual := getBIOSinfo()
-	assert.IsType(t, expected, actual, "`getBIOSinfo` should return a `biosinfo` type")
+	assert.IsType(t, expected, actual, "`getBIOSinfo()` should return a `biosinfo` type")
+	assert.IsType(t, expected.boardName, actual.boardName, "`getBIOSinfo()` should return a `boardName` field with a string type")
+	assert.IsType(t, expected.boardVendor, actual.boardVendor, "`getBIOSinfo()` should return a `boardVendor` field with a string type")
+	assert.IsType(t, expected.biosVendor, actual.biosVendor, "`getBIOSinfo()` should return a `biosVendor` field with a string type")
+	assert.IsType(t, expected.biosVersion, actual.biosVersion, "`getBIOSinfo()` should return a `biosVersion` field with a string type")
+	assert.IsType(t, expected.biosDate, actual.biosDate, "`getBIOSinfo()` should return a `biosDate` field with a string type")
 }
--- disk_test.go
+++ disk_test.go
@@ -6,23 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetDiskinfo(t *testing.T) {
+// TestGetDiskinfoType test if `getDiskinfo()` return `[]diskinfo` slice" and if each fields have the correct types
+// Types regression testing
+func TestGetDiskinfoType(t *testing.T) {
 	expected := []diskinfo{
 		{
-			device:      "abc",
-			path:        "abc",
-			total:       string(123),
-			used:        string(123),
-			usedPercent: int(100),
-		},
-		{
-			device:      "abc",
-			path:        "abc",
-			total:       string(123),
-			used:        string(123),
-			usedPercent: int(100),
+			device:      "", // the result values of the fields are not tested
+			path:        "",
+			total:       "",
+			used:        "",
+			usedPercent: int(0),
 		},
 	}
 	actual := getDiskinfo()
 	assert.IsType(t, expected, actual, "`getDiskinfo()` should return a `[]diskinfo` slice")
+	assert.IsType(t, expected[0].device, actual[0].device, "`getDiskinfo()` should return a `device` field with a string type")
+	assert.IsType(t, expected[0].path, actual[0].path, "`getDiskinfo()` should return a `path` field with a string type")
+	assert.IsType(t, expected[0].total, actual[0].total, "`getDiskinfo()` should return a `total` field with a string type")
+	assert.IsType(t, expected[0].used, actual[0].used, "`getDiskinfo()` should return a `used` field with a string type")
+	assert.IsType(t, expected[0].usedPercent, actual[0].usedPercent, "`getDiskinfo()` should return a `usedPercent` field with an int type")
 }
