1fcbc7de989281fb0be2714b4a513e48d56fdff5
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-26 14:04:33 +0200

    Update testing

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,13 +12,19 @@ func TestInt8SliceToString(t *testing.T) {
 	expected := "abc"
 	actual := int8SliceToString([]int8{97, 98, 99}) // See the ASCII table: 97 = "a" ; 98 ="b" ; 99 = "c"
 
-	assert.IsType(t, expected, actual, "`getNetinfo()` should return a `string`")
+	assert.IsType(t, expected, actual, "`int8SliceToString()` should return a `string`")
 	assert.Equal(t, expected, actual, "`int8SliceToString([]int8{97, 98, 99})` should be equal to \"abc\"")
 }
 
 // TestReadAndTrimFile test the returned type and value of `readAndTrimFile()`
-// func TestReadAndTrimFile(t *testing.T) {
-// }
+func TestReadAndTrimFile(t *testing.T) {
+	expected := "package main"
+	actual := readAndTrimFile("utils_test.go")
+
+	assert.IsType(t, expected, actual, "`readAndTrimFile()` should return a `string`")
+	assert.Regexp(t, regexp.MustCompile(expected), actual, "`readAndTrimFile(\"utils_test.go\")` should contain \"package main\"")
+	//TODO improve tests with Equal instead of Regexp by using a temp file ?
+}
 
 // TODO maybe stick to the testing stdlib intead of testify
 // func TestInt8SliceToString(t *testing.T) {
