106f69ee8c0246266cffbc57d1d9210cd2f66c41
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-26 12:23:29 +0200

    Update testing

--- process_test.go
+++ process_test.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 
+	"github.com/shirou/gopsutil/process"
 	"github.com/stretchr/testify/assert"
 )
 
+// TestGetProcinfo test the returned fields values and types of `getProcinfo()`
 func TestGetProcinfo(t *testing.T) {
+	// setup the faking of `process.Pids()` & `process.NewProcess()`
+	oldProcessPids := processPids
+	oldProcStatus := procStatus
+	processPids = func() ([]int32, error) {
+		ret := []int32{1, 2, 3} // 3 fakes pids
+		return ret, nil
+	}
+	procStatus = func(proc *process.Process) (string, error) {
+		return "R", nil // all processes have the "R" status
+	}
+
+	// test
 	expected := procinfo{
-		total:   string(100),
-		running: string(100),
+		total:   strconv.FormatInt(3, 10),
+		running: strconv.FormatInt(3, 10),
 	}
 	actual := getProcinfo()
+	assert.Equal(t, expected, actual, "`getProcinfo` should be equal to main.procinfo{total:\"3\", running:\"3\"}")
+
+	// teardown
+	processPids = oldProcessPids
+	procStatus = oldProcStatus
+}
+
+// TestGetProcinfoType test if `getProcinfo()` return a `procinfo` type and if each fields have the correct types
+func TestGetProcinfoType(t *testing.T) {
+	expected := procinfo{
+		total:   "", // the result values of the fields are not tested
+		running: "",
+	}
+	actual := getProcinfo()
+
 	assert.IsType(t, expected, actual, "`getProcinfo` should return a `procinfo` type")
+	assert.IsType(t, expected.total, actual.total, "`getMeminfo()` should return a `total` field with a string type")
+	assert.IsType(t, expected.running, actual.running, "`getMeminfo()` should return a `ramUsed` field with a string type")
 }
