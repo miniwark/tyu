74d5fbd3e8e67e5b76d9ac602d307293234654db
Bertrand Lecervoisier <miniwark@gmail.com>
2016-04-27 20:29:39 +0200

    Code refactoring

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 
-	ui "github.com/gizak/termui"
 	flag "github.com/ogier/pflag"
 )
 
@@ -36,112 +34,6 @@ func main() {
 		os.Exit(0)
 	}
 
-	// init termui
-	if err := ui.Init(); err != nil {
-		panic(err) //TODO do not panic but manage the error
-	}
-	defer ui.Close()
-
-	ramGauge := createRAMGauge()
-	swapGauge := createSwapGauge()
-	cpuGauge := createCPUGauge()
-
-	// display informations about the physical disks
-	diskGauges := make([]*ui.Gauge, 3) // 3 disks max
-	for i := range diskGauges {        //TEMP range = 3
-		diskGauges[i] = ui.NewGauge()
-		diskGauges[i].BarColor = ui.ColorBlue
-		diskGauges[i].Width = 39
-		diskGauges[i].Height = 3
-		diskGauges[i].X = 0
-		diskGauges[i].Y = 9 + (i * 3)
-	}
-
-	netinfo := createNetList()
-	procinfo := createProcList()
-	hostinfo := createHostList()
-	cpuinfo := createCPUList()
-	biosinfo := createBIOSList()
-
-	quit := createQuitPar()
-
-	// variables to calculate network traffic (to avoid cumulative display)
-	var networkOld, networkNew Netinfo
-
-	// render the dashboard with 26x80 fixed size
-	draw := func(t int) {
-		// update memory usage gauges
-		mem := getMeminfo()
-		ramGauge.Percent = mem.ramUsedPercent
-		ramGauge.Label = "{{percent}}% - " + mem.ramUsed + "/" + mem.ramTotal + " GiB"
-		swapGauge.Percent = mem.swapUsedPercent
-		swapGauge.Label = "{{percent}}% - " + mem.swapUsed + "/" + mem.swapTotal + " GiB"
-
-		// update CPU usage gauge
-		cpuGauge.Percent = getCPUpercent()
-
-		// update disks usage gauges
-		disk := getDiskinfo()
-		for i := range disk {
-			if i >= 3 { // display 3 disk max
-				break
-			}
-			diskGauges[i].BorderLabel = disk[i].device + " disk usage "
-			diskGauges[i].Percent = disk[i].usedPercent
-			diskGauges[i].Label = "{{percent}}% - " + disk[i].used + "/" + disk[i].total + " GiB"
-		}
-
-		// update network informations
-		net := getNetinfo()
-		networkNew.up = net.up - networkOld.up
-		networkNew.down = net.down - networkOld.down
-		networkOld = net
-		netinfo.Items[0] = "[Up   ](fg-cyan)" + strconv.FormatFloat(networkNew.up, 'f', 1, 64) + " KiB"
-		netinfo.Items[1] = "[Down ](fg-cyan)" + strconv.FormatFloat(networkNew.down, 'f', 1, 64) + " KiB"
-
-		// update processes informations
-		procs := getProcinfo()
-		procinfo.Items[0] = "[Tasks   ](fg-cyan)" + procs.total
-		procinfo.Items[1] = "[Running ](fg-cyan)" + procs.running
-
-		// update the host informations
-		uptime := getUptime()
-		hostinfo.Items[7] = "[Uptime           ](fg-cyan)" + uptime
-
-		// update the CPUs informations
-		cpuinfo.Items[4] = "[Temperature ](fg-cyan)" + "--"
-
-		// register the gauges and blocks to the renderer
-		ui.Render(
-			ramGauge,
-			swapGauge,
-			cpuGauge,
-			diskGauges[0],
-			diskGauges[1],
-			diskGauges[2],
-			netinfo,
-			procinfo,
-			hostinfo,
-			cpuinfo,
-			biosinfo,
-			quit,
-		)
-	}
-
-	// quit on `q` keystroke handler
-	ui.Handle("/sys/kbd/q", func(ui.Event) {
-		ui.StopLoop()
-	})
-	// quit on `CTRL+c` keystroke handler
-	ui.Handle("/sys/kbd/C-c", func(ui.Event) {
-		ui.StopLoop()
-	})
-	// timer handler to refresh every second
-	ui.Handle("/timer/1s", func(e ui.Event) {
-		t := e.Data.(ui.EvtTimer)
-		draw(int(t.Count))
-	})
-	ui.Loop()
+	// init and display the dashboard
+	dashboard()
 }
-
-// the dashboard is 26x80
--- ui.go
+++ ui.go
@@ -1,6 +1,10 @@
 package main
 
-import ui "github.com/gizak/termui"
+import (
+	"strconv"
+
+	ui "github.com/gizak/termui"
+)
 
 // https://github.com/divan/expvarmon/blob/master/ui_single.go
 // https://github.com/mhoc/river/blob/master/src/river/send_box.go
@@ -17,6 +21,14 @@ func createRAMGauge() *ui.Gauge {
 	return g
 }
 
+// updateRAMGauge update the percentage and label of the RAM gauge
+func updateRAMGauge(g *ui.Gauge) {
+	mem := getMeminfo() // TODO replace getMeminfo() by two separate types & functions, or merge updateRAMGauge & updateSwapGauge to a generic func
+
+	g.Percent = mem.ramUsedPercent
+	g.Label = "{{percent}}% - " + mem.ramUsed + "/" + mem.ramTotal + " GiB"
+}
+
 // createSwapGauge display physical swap usage informations
 func createSwapGauge() *ui.Gauge {
 	g := ui.NewGauge()
@@ -29,6 +41,14 @@ func createSwapGauge() *ui.Gauge {
 	return g
 }
 
+// updateRAMGauge update the percentage and label of the Swap gauge
+func updateSwapGauge(g *ui.Gauge) {
+	swap := getMeminfo() // TODO replace getMeminfo() by two separate types & functions, or merge updateRAMGauge & updateSwapGauge to a generic func
+
+	g.Percent = swap.swapUsedPercent
+	g.Label = "{{percent}}% - " + swap.ramUsed + "/" + swap.ramTotal + " GiB"
+}
+
 // createCPUGauge display information about the CPU usage
 func createCPUGauge() *ui.Gauge {
 	g := ui.NewGauge()
@@ -39,6 +59,46 @@ func createCPUGauge() *ui.Gauge {
 	g.X = 0
 	g.Y = 6
 	return g
+}
+
+// updateRAMGauge update the percentage of the CPU gauge
+func updateCPUGauge(g *ui.Gauge) {
+	g.Percent = getCPUpercent()
+}
+
+// createDiskGauges display informations about the physical disks
+// up to 3 disks. return an arrau of termui.Gauge
+func createDiskGauge() []*ui.Gauge {
+	disk := getDiskinfo()
+
+	g := make([]*ui.Gauge, 3) // display  3 disks max
+
+	for i := range disk {
+		if i >= 3 { // display 3 disk max
+			break
+		}
+		g[i] = ui.NewGauge()
+		g[i].BarColor = ui.ColorBlue
+		g[i].Width = 39
+		g[i].Height = 3
+		g[i].X = 0
+		g[i].Y = 9 + (i * 3)
+	}
+	return g
+}
+
+// updateRAMGauge update the percentages of the disk gauges
+func updateDiskGauge(g []*ui.Gauge) { //BUG mounting an additional disk wile runing tyu make it crask
+	disk := getDiskinfo()
+
+	for i := range disk {
+		if i >= 3 { // display 3 disk max
+			break
+		}
+		g[i].BorderLabel = disk[i].device + " disk usage "
+		g[i].Percent = disk[i].usedPercent
+		g[i].Label = "{{percent}}% - " + disk[i].used + "/" + disk[i].total + " GiB"
+	}
 }
 
 // createNetList display information about the global network activity (all interfaces)
@@ -56,6 +116,20 @@ func createNetList() *ui.List { //TODO move over ramGauge
 	return l
 }
 
+// NetworkOld keep track of network trafic up & down
+var NetworkOld Netinfo //TODO get rid of this globals
+
+//  updateNetList update network informations
+func updateNetList(g *ui.List) {
+	net := getNetinfo()
+	up := net.up - NetworkOld.up
+	down := net.down - NetworkOld.down
+	NetworkOld = net
+
+	g.Items[0] = "[Up   ](fg-cyan)" + strconv.FormatFloat(up, 'f', 1, 64) + " KiB"
+	g.Items[1] = "[Down ](fg-cyan)" + strconv.FormatFloat(down, 'f', 1, 64) + " KiB"
+}
+
 // createProcList display information about the processes
 func createProcList() *ui.List { //TODO move over ramGauge
 	l := ui.NewList()
@@ -69,6 +143,13 @@ func createProcList() *ui.List { //TODO move over ramGauge
 	l.X = 20
 	l.Y = 18
 	return l
+}
+
+// updateProcList update processes informations
+func updateProcList(g *ui.List) {
+	procs := getProcinfo()
+	g.Items[0] = "[Tasks   ](fg-cyan)" + procs.total
+	g.Items[1] = "[Running ](fg-cyan)" + procs.running
 }
 
 // createHostList display system informations about the host
@@ -94,6 +175,12 @@ func createHostList() *ui.List {
 	return l
 }
 
+// 	updateHostList/ update the host informations
+func updateHostList(g *ui.List) {
+	uptime := getUptime()
+	g.Items[7] = "[Uptime           ](fg-cyan)" + uptime
+}
+
 // createCPUList display informations about the CPUs
 func createCPUList() *ui.List {
 	cpu := getCPUinfo()
@@ -105,13 +192,19 @@ func createCPUList() *ui.List {
 		"[Vendor      ](fg-cyan)" + cpu.vendorID,
 		"[Model       ](fg-cyan)" + cpu.modelName, //TODO use refreshing rate to display roll long text ?
 		"[Frequency   ](fg-cyan)" + cpu.cpuMhz + " Mhz",
-		"[Temperature ](fg-cyan)", //TODO
+		"[Temperature ](fg-cyan)", //TODO CPU temperature
 	}
 	l.Width = 39
 	l.Height = 7
 	l.X = 40
 	l.Y = 10
 	return l
+}
+
+// 	updateCPUList update the CPU informations
+func updateCPUList(g *ui.List) {
+	//uptime := getCPUtemp() //TODO CPU temperature
+	g.Items[4] = "[Temperature ](fg-cyan)" + "--"
 }
 
 // createBIOSList display bios and motherboard informations
@@ -142,4 +235,77 @@ func createQuitPar() *ui.Par {
 	p.Y = 23
 	p.Border = false
 	return p
+}
+
+// init the termui dashboard
+func dashboard() {
+	// init the dashboard
+	if err := ui.Init(); err != nil {
+		panic(err) //TODO do not panic but manage the error
+	}
+	defer ui.Close()
+
+	// inits the dashboard widgets
+	ramGauge := createRAMGauge()
+	swapGauge := createSwapGauge()
+	cpuGauge := createCPUGauge()
+	diskGauge := createDiskGauge()
+	netList := createNetList()
+	procList := createProcList()
+	hostList := createHostList()
+	cpuList := createCPUList()
+	biosList := createBIOSList()
+	quitPar := createQuitPar()
+
+	// render the dashboard with 26x80 fixed size
+	draw := func(t int) {
+		// updates the dashboard widgets
+		updateRAMGauge(ramGauge)
+		updateSwapGauge(swapGauge)
+		updateCPUGauge(cpuGauge)
+		updateDiskGauge(diskGauge)
+		updateNetList(netList)
+		updateProcList(procList)
+		updateHostList(hostList)
+		updateCPUList(cpuList)
+
+		// register the dashboard widgets (except the disks)
+		ui.Render(
+			ramGauge,
+			swapGauge,
+			cpuGauge,
+			netList,
+			procList,
+			hostList,
+			cpuList,
+			biosList,
+			quitPar,
+		)
+
+		// register the dashboard disk gauges widgets
+		disk := getDiskinfo() // TODO try to avoid this check
+		if len(disk) >= 1 {   // one disk or more
+			ui.Render(diskGauge[0])
+		}
+		if len(disk) >= 2 { // two disks or more
+			ui.Render(diskGauge[1])
+		}
+		if len(disk) == 3 { // three disks
+			ui.Render(diskGauge[2])
+		}
+	}
+
+	// setup the events handlers
+	ui.Handle("/sys/kbd/q", func(ui.Event) { // quit on `q` keystroke
+		ui.StopLoop()
+	})
+	ui.Handle("/sys/kbd/C-c", func(ui.Event) { // quit on `CTRL+c` keystroke
+		ui.StopLoop()
+	})
+	ui.Handle("/timer/1s", func(e ui.Event) { // refresh every second
+		t := e.Data.(ui.EvtTimer)
+		draw(int(t.Count))
+	})
+
+	ui.Loop()
 }
